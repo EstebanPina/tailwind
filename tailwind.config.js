b5f00f061ef6bd2456edfdb9d8d647245169be5b
EstebanPina <esteban0400@gmail.com>
2022-07-11 15:26:54 -0500

    Final Commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,16 @@
-module.export={
-    purge:['./public/**/*.html','./src/**/*.js'],
-    darkmode:false,
-    theme:{
-        extend:{}
+module.exports = {
+    content: ["./public/**/*.{html,js}"],
+    theme: {
+      extend: {
+        backgroundImage: theme => ({
+            'wave-patern': "url('../img/wave-white.png')",
+            'wave-patter-dots': "url('../img/pattern-white-dots.png')",
+            'pattern-characters-red': "url('../img/pattern-characters-red.png')",
+            'characters-mario': "url('../img/character-l.png')",
+            'wave-pink': "url('../img/wave-pink.png')",
+            'yellow-dots': "url('../img/pattern-yellow-dots.png')",
+        }),
+      },
     },
-    variants:{
-        extend:{}
-    },
-    plugins:[],
-}
+    plugins: [],
+  }
